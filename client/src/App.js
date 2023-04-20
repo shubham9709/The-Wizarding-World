fd97a73d851fe1ee583f9da9d2820d301eeed5e6
Shubham <shubham@pop-os.localdomain>
2023-04-20 16:06:06 +0530

    Starting a wizarding project

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <div id="title">
+        <span>HARRY POTTER</span>
+        <br />
+        <span>and the wizarding world</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
